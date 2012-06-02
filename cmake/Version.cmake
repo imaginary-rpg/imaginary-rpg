f28dce3f81ffd4a6d3a128d276e1fefbc07f9037
Patrick M. Niedzielski <PatrickNiedzielski@gmail.com>
2012-06-02 23:25:30 +0000

    Initial build scripts and game main loop.  I'll make this easier to run in a bit.

---
+++ cmake/Version.cmake
@@ -0,0 +1,28 @@
+# Imaginary: A Free Software Adventure RPG
+# Copyright (C) 2012, the people listed in the AUTHORS file.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Only edit the ones below:
+set (IMAGINARY_VERSION_MAJOR "0")
+set (IMAGINARY_VERSION_MINOR "1")
+set (IMAGINARY_NAME "Imaginary")
+set (IMAGINARY_NAME_SHORT "imaginary")
+set (IMAGINARY_DESCRIPTION "A FOSS adventure RPG.")
+set (IMAGINARY_WEBSITE "http://imaginary-rpg.github.com/imaginary-rpg/")
+
+# These will be generated automatically from the above information.
+set (IMAGINARY_VERSION
+     "${IMAGINARY_VERSION_MAJOR}.${IMAGINARY_VERSION_MINOR}")
+set (IMAGINARY_NAME_RELEASE "${IMAGINARY_NAME_SHORT}-${IMAGINARY_VERSION}")
